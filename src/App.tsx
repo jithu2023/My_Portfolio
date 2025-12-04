4db02aa5f792829165a0a3b6a21e938c358ae700
Jithumon Jacob <jithumon.j@kalvium.community>
2025-12-05 00:17:28 +0530

    hero section added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import Navbar from "../src/components/Navbar";
+import Hero from "../src/components/Hero";
 
 function App() {
   return (
-  <div
-  className="min-h-screen w-full bg-no-repeat bg-cover bg-top"
-  style={{
-    backgroundImage: "url('/src/assets/landing-bg.jpg')",
-  }}
->
-
+    <div
+      className="min-h-screen w-full bg-no-repeat bg-cover bg-top"
+      style={{
+        backgroundImage: "url('/src/assets/landing-bg.jpg')",
+      }}
+    >
       <Navbar />
+      <Hero />
 
       <div className="flex items-center justify-center min-h-screen">
         <h1 className="text-4xl font-bold text-white"></h1>
